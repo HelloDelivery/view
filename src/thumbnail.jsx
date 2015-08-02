564d36dc11457f4abcc2ebd133b956cd15d603ee
cutofmyjib <thedianalozano@gmail.com>
2015-08-02 10:30:03 -0700

    changed some layout

--- src/thumbnail.jsx
+++ src/thumbnail.jsx
@@ -2,22 +2,16 @@ var React = require('react');
 
 module.exports = React.createClass({
 
-
-
   render: function(){
     console.log(this.props);
     return <div className="col-xs-6 col-md-3">
       <div className="thumbnail">
-      <div className="media">
-        <a className="pull-left">
-          <img className="media-object" src={this.props.thumb} />
-        </a>
-        <div className="media-body">
-          <h4 className="media-heading">{this.props.name}</h4>
-          <p>{this.props.date}</p>
-          <audio controls><source src={this.props.audio} type="audio/mp3" /></audio>
+        <div className="media">
+          <div className="media-body">
+            <h4 className="media-heading">{this.props.delivery.manifest.description}</h4>
+            <p>{this.props.delivery.status}</p>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   }
