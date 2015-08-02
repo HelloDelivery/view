71e591fc1e446165fa17fb45bd1c501d708a24a0
Claudiu Andrei <claudiu.andrei@docusign.com>
2015-08-02 12:44:25 -0700

    Add map

--- src/thumbnail.jsx
+++ src/thumbnail.jsx
@@ -4,10 +4,11 @@ var moment = require('moment');
 module.exports = React.createClass({
 
   render: function() {
+    var deadline = this.props.delivery.dropoff_eta || this.props.delivery.updated;
     return <article>
       <div>
         {this.props.delivery.manifest.description}
-        [{moment(this.props.delivery.dropoff_eta).format('HH:mm')}]
+        [{moment(deadline).format('HH:mm')}]
       </div>
     </article>
   }
